8dc25dbac1f2876662be343cc0e69dbe2172fb84
Albertukraine <101990726+Albertukraine@users.noreply.github.com>
2022-10-31 00:20:59 +0200

    add prettier

--- src/components/FeedBackButtons/FeedBackButtons.jsx
+++ src/components/FeedBackButtons/FeedBackButtons.jsx
@@ -1,29 +1,27 @@
 import PropTypes from 'prop-types';
 import style from './FeedBackButtons.module.css';
 
-export const FeedBackOptions = (props) => {
-const {name, neutralHandler, badHandler, goodHandler} = props;
-   //  console.log("BTNprops",props);
-    return ( <div className={style.btnBox}>
-         <button className={style.button} type="button" onClick={goodHandler}>
+export const FeedBackOptions = props => {
+  const { name, neutralHandler, badHandler, goodHandler } = props;
+  //  console.log("BTNprops",props);
+  return (
+    <div className={style.btnBox}>
+      <button className={style.button} type="button" onClick={goodHandler}>
         {name[0]}
       </button>
-         <button className={style.button} type="button" onClick={neutralHandler}>
-         {name[1]}
+      <button className={style.button} type="button" onClick={neutralHandler}>
+        {name[1]}
       </button>
-         <button className={style.button} type="button" onClick={badHandler}>
-         {name[2]}
+      <button className={style.button} type="button" onClick={badHandler}>
+        {name[2]}
       </button>
-      
     </div>
-     
-    )
-  };
+  );
+};
 
-  FeedBackOptions.propTypes = {
-   name: PropTypes.array,
-   neutralHandler: PropTypes.func,
-   badHandler: PropTypes.func,
-   goodHandler: PropTypes.func,
-
-  }
+FeedBackOptions.propTypes = {
+  name: PropTypes.array,
+  neutralHandler: PropTypes.func,
+  badHandler: PropTypes.func,
+  goodHandler: PropTypes.func,
+};
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import style from './Notification.module.css';
 
-export const Notification = (props) => {
-const {message} = props;
-   //  console.log("BTNprops",props);
-    return ( <p className={style.notificaton}>{message}</p>
-     
-    )
-  };
+export const Notification = props => {
+  const { message } = props;
+  //  console.log("BTNprops",props);
+  return <p className={style.notificaton}>{message}</p>;
+};
 
-  Notification.propTypes = {
-    message: PropTypes.string,
-  }
+Notification.propTypes = {
+  message: PropTypes.string,
+};
